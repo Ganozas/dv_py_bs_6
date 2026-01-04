d12bc1c477c6cc516167ae1b9d3c05641153cf5d
Ganozas <leha.samsonov.200@gmail.com>
2026-01-04 13:26:48 +0700

    FIx defects

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 
 def is_very_long(password):
-    if len(password) <= 11:
-	    return False
-    else:
-	    return True
-
+    return len(password) >=  12
+    
 
 def has_digit(password):
     return any(i.isdigit() for i in password)
@@ -18,31 +15,28 @@ def has_lower_letters(password):
     return any (i.islower() for i in password)
 
 
-def has_sambols(password):
+ def has_sambols(password):
     return any (not i.isdigit() and not i.isalpha() for i in password)
 
 
-def main():
-    is_very_long()
-    has_digit()
-    has_upper_letters()
-    has_lower_letters()
-    has_sambols()
+def has_latters(password):
+    return any (i.isalpha() for i in password)
 
 
 if __name__ == '__main__':
     password = (input("Введите пароль: "))
     score = 0
 
-    function_list = [is_very_long,
-                 has_digit,
-                 has_lower_letters,
-                 has_upper_letters,
-                 has_sambols,
+    function_list = [
+        is_very_long,
+        has_digit,
+        has_lower_letters,
+        has_upper_letters,
+        has_sambols,
 ]
 
     for i in function_list:
-        result = (i(password))
+        result = i(password)
         if result:
             score += 2
    	
